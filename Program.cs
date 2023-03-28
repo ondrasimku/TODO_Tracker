14c26619cfc2d443f3dec934b91587a7f8f8eb62
Ondřej Šimků <simkuond@fit.cvut.cz>
2023-03-28 20:49:01 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TODO_Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
